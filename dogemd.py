a95a93bcd3a3ab78f2072cc37bae639e21f6c710
Qbius <theqbius@gmail.com>
2018-11-05 02:53:59 +0100

    slight file refactoring

---
+++ dogemd.py
@@ -0,0 +1,8 @@
+from sys import argv
+from dogecore import get_doge
+if len(argv) == 4:
+    texttop, textbot, fontsizestr = argv[1:]
+    get_doge(texttop, textbot, int(fontsizestr))
+elif len(argv) == 3:
+    texttop, textbot = argv[1:]
+    get_doge(texttop, textbot, 48)
--- doje.py
+++ doje.py
@@ -1,47 +1,9 @@
-from PIL import Image, ImageDraw, ImageFont
-import time
-from random import choice
-from os import listdir
-image = Image.open("images/" + choice(listdir("images")))
-image_width, image_height = image.size
-draw = ImageDraw.Draw(image)
-
-def get_fonts(fontsize, texttop, textbot, fonttop_name = "impact.ttf", fontbot_name = "impact.ttf"):
-    fonttop = ImageFont.truetype(fonttop_name, fontsize)
-    fontbot = ImageFont.truetype(fontbot_name, fontsize)
-    while draw.textsize(texttop, fonttop)[0] >= image_width:
-        fontsize -= 5
-        fonttop = ImageFont.truetype(fonttop_name, fontsize)
-    while draw.textsize(textbot, fontbot)[0] >= image_width:
-        fontsize -= 5
-        fontbot = ImageFont.truetype(fontbot_name, fontsize)
-    return fonttop, fontbot
-
-def draw_text_with_shadow(x, y, text, font, shadow_color = "black", text_color = "white"):
-    draw.text((x - 1, y - 1), text, font=font, fill=shadow_color)
-    draw.text((x + 1, y - 1), text, font=font, fill=shadow_color)
-    draw.text((x - 1, y + 1), text, font=font, fill=shadow_color)
-    draw.text((x + 1, y + 1), text, font=font, fill=shadow_color)
-    draw.text((x, y), text=text, font=font, fill=text_color)
-
-def insert_text(fonttop, fontbot, texttop, textbot, shadow_color = "black"):
-    text_width1, text_height1 = draw.textsize(texttop, fonttop)
-    x1 = (image_width - text_width1) / 2
-    y1 = text_height1 * 0.25
-
-    text_width2, text_height2 = draw.textsize(textbot, fontbot)
-    x2 = (image_width - text_width2) / 2
-    y2 = image_height - text_height2 * 1.5
-
-    draw_text_with_shadow(x1, y1, texttop, fonttop, shadow_color)
-    draw_text_with_shadow(x2, y2, textbot, fontbot, shadow_color)
+from dogecore import get_doge
 
 texttop = input("Top text? \n").upper()
 textbot = input("Bottom text? \n").upper()
 fontsize = int(input("Font size? (48 recommended) \n"))
 if fontsize > 64:
     fontsize = 64
-fonttop, fontbot = get_fonts(fontsize, texttop, textbot)
-insert_text(fonttop, fontbot, texttop, textbot)
-image.show()
-image.save("images/results/" + str(int(time.time())) + ".jpg")
+
+get_doge(texttop, textbot, fontsize)
