488526e873548c1c1baafba13b58f6586e035563
Dulet <duleet@gmail.com>
2018-11-05 01:57:47 +0100

    le update has arrived

--- doje.py
+++ doje.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import random
+import random, time
 dab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] #trivial but works
 a = random.choice(dab)
 path = "images/"
@@ -13,29 +13,38 @@ textbot = input("Bottom text? \n").upper()
 size = int(input("Font size? (48 recommended) \n"))
 if size > 64:
     size = 64
-font = ImageFont.truetype("impact.ttf", size)
+fonttop = ImageFont.truetype("impact.ttf", size)
+fontbot = ImageFont.truetype("impact.ttf", size)
 
-def bottom_text(img, font, texttop, textbot, shadowcolor):
+while ImageDraw.Draw(image).textsize(texttop, fonttop)[0] >= image_width:
+    size -= 10
+    fonttop = ImageFont.truetype("impact.ttf", size)
+while ImageDraw.Draw(image).textsize(textbot, fontbot)[0] >= image_width:
+    size -= 10
+    fontbot = ImageFont.truetype("impact.ttf", size)
+
+def bottom_text(img, fonttop, fontbot, texttop, textbot, shadowcolor):
     draw = ImageDraw.Draw(img)
-    text_width, text_height = draw.textsize(texttop, font)
+    text_width, text_height = draw.textsize(texttop, fonttop)
     x = (image_width-text_width)/2
     y = 0 + text_height*0.25
-    draw.text((x - 1, y - 1), texttop, font=font, fill=shadowcolor)
-    draw.text((x + 1, y - 1), texttop, font=font, fill=shadowcolor)
-    draw.text((x - 1, y + 1), texttop, font=font, fill=shadowcolor)
-    draw.text((x + 1, y + 1), texttop, font=font, fill=shadowcolor)
-    draw.text((x, y), text=texttop, fill="white", font=font)
+    draw.text((x - 1, y - 1), texttop, font=fonttop, fill=shadowcolor)
+    draw.text((x + 1, y - 1), texttop, font=fonttop, fill=shadowcolor)
+    draw.text((x - 1, y + 1), texttop, font=fonttop, fill=shadowcolor)
+    draw.text((x + 1, y + 1), texttop, font=fonttop, fill=shadowcolor)
+    draw.text((x, y), text=texttop, fill="white", font=fonttop)
 
-    text_width, text_height = draw.textsize(textbot, font)
+    text_width, text_height = draw.textsize(textbot, fontbot)
     x1 = (image_width - text_width) / 2
     y1 = image_height - text_height*1.5
-    draw.text((x1 - 1, y1 - 1), textbot, font=font, fill=shadowcolor)
-    draw.text((x1 + 1, y1 - 1), textbot, font=font, fill=shadowcolor)
-    draw.text((x1 - 1, y1 + 1), textbot, font=font, fill=shadowcolor)
-    draw.text((x1 + 1, y1 + 1), textbot, font=font, fill=shadowcolor)
-    draw.text((x1, y1), text=textbot, fill="white", font=font)
+    draw.text((x1 - 1, y1 - 1), textbot, font=fontbot, fill=shadowcolor)
+    draw.text((x1 + 1, y1 - 1), textbot, font=fontbot, fill=shadowcolor)
+    draw.text((x1 - 1, y1 + 1), textbot, font=fontbot, fill=shadowcolor)
+    draw.text((x1 + 1, y1 + 1), textbot, font=fontbot, fill=shadowcolor)
+    draw.text((x1, y1), text=textbot, fill="white", font=fontbot)
     return img
 
 
-bottom_text(image, font, texttop, textbot, shadowcolor)
+bottom_text(image, fonttop, fontbot, texttop, textbot, shadowcolor)
 image.show()
+image.save("images/results/" + str(int(time.time())) + ".jpg")
